adf0cec7fefff5d1275a9990f4651d1177a25df6
Khaliddxx <doodz@aucegypt.edu>
2021-06-09 03:29:46 +0200

    Done with main requirement, time to further explore styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from "./logo.svg";
 import "./App.css";
+import "./style.css";
 import imageInSrc from "./imageInSrc.jpg";
 
 function App() {
   return (
     <>
       <div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
-        <h1 className="title red">Your name here</h1>
 
-        <br></br>
+      {/* <div className="skeleton" > */}
+        <h1 className="title red">Khalid A. Mohamed</h1>
+        <a className="twt" href="https://twitter.com/khalidxah">Twitter Account</a>
 
-        <img src={imageInSrc} />
+        <br />
 
-        <br></br>
+        <img className="imginsrc" src={imageInSrc} />
 
-        <img src="/imageInPublic.jpg" />
+
+        <img className="imginpublic" src="/imageInPublic.jpg" />
       </div>
 
-      <video width="320" height="240" controls>
-        <source src="myVideo.mp4" type="video/mp4" />
+      <video className='vid' width="320" height="240" controls>
+        <source src="/myVideo.mp4" type="video/mp4" />
       </video>
     </>
   );
